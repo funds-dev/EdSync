2b39a33234262cea2aca32bf6e21a291b4b23443
Funds <fundsfuneral@gmail.com>
2025-03-12 22:08:45 -0700

    Create TitleCard.swift

---
+++ Components/TitleCard.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct TitleCard: View {
+    var numberOnline = 20
+    var body : some View {
+        VStack (alignment: .leading) {
+            Text("EdSync Chatroom")
+                .font(.title).bold()
+            
+            Text("\(numberOnline)")
+                .font(.caption)
+                .foregroundStyle(.green) + Text(" Members Testing").font(.caption)
+                .foregroundStyle(.gray)
+        }
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
